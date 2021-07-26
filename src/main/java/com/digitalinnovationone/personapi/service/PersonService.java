b773f09deb7bdb77a04579b570efd059016348c8
LohanLima <lohanmlima@gmail.com>
2021-07-26 12:32:31 -0300

    26/07 12:31 erro compile, try to path lombok and mapstruct

--- src/main/java/com/digitalinnovationone/personapi/service/PersonService.java
+++ src/main/java/com/digitalinnovationone/personapi/service/PersonService.java
@@ -9,6 +9,10 @@ import com.digitalinnovationone.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
 public class PersonService {
     private PersonRepository personRepository;
@@ -29,5 +33,17 @@ public class PersonService {
                 .builder()
                 .message("Created Person with ID" + savePerson.getId())
                 .build();
+    }
+
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    public PersonDTO findeById(Long id) {
+        Optional<Person> optionalPerson = personRepository.findById();
     }
 }
