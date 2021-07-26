323e101a6bc0545e1b964f470bb37cec1690765e
LohanLima <lohanmlima@gmail.com>
2021-07-26 10:15:13 -0300

    final atual

--- src/main/java/com/digitalinnovationone/personapi/service/PersonService.java
+++ src/main/java/com/digitalinnovationone/personapi/service/PersonService.java
@@ -4,6 +4,7 @@ package com.digitalinnovationone.personapi.service;
 import com.digitalinnovationone.personapi.dto.request.PersonDTO;
 import com.digitalinnovationone.personapi.dto.response.MessageResponseDTO;
 import com.digitalinnovationone.personapi.entity.Person;
+import com.digitalinnovationone.personapi.mapper.PersonMapper;
 import com.digitalinnovationone.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,8 @@ import org.springframework.stereotype.Service;
 public class PersonService {
     private PersonRepository personRepository;
 
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
     @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
@@ -19,14 +22,9 @@ public class PersonService {
 
     public MessageResponseDTO createPerson(PersonDTO personDTO) {
 
-        Person personToSave = Person.builder()
-                .firstName(personDTO.getFirstName())
-                .lastName(personDTO.getFirstName())
-                .birthData(personDTO.getBirthData())
-                .phones(personDTO.getPhones())
-                .build();
+        Person personToSave = personMapper.toModel(personDTO);
 
-        Person savePerson = personRepository.save(personDTO);
+        Person savePerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Created Person with ID" + savePerson.getId())
